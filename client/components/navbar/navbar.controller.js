254686d59f97d599dbeebdad6aa55162103fa154
AndreiMarin <hedrox53@gmail.com>
2015-07-30 13:51:18 +0300

    Sidebar + Navbar

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('schoolPlannerApp')
-  .controller('NavbarCtrl', function ($scope, $location, Auth) {
+  .controller('NavbarCtrl', function ($scope, $location, Auth, $mdSidenav) {
     $scope.menu = [{
       'title': 'Home',
       'link': '/'
@@ -11,11 +11,16 @@ angular.module('schoolPlannerApp')
     $scope.isLoggedIn = Auth.isLoggedIn;
     $scope.isAdmin = Auth.isAdmin;
     $scope.getCurrentUser = Auth.getCurrentUser;
+      $scope.toggleList = toggleUsersList;
 
     $scope.logout = function() {
       Auth.logout();
       $location.path('/login');
     };
+      function toggleUsersList()
+      {
+          $mdSidenav('left').toggle();
+      }
 
     $scope.isActive = function(route) {
       return route === $location.path();
