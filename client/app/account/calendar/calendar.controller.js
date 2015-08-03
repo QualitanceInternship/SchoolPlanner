0f8b1c5f6d0b490236b7dfd70538ffd1c741fe36
diaconubogdan <body@darkstrike.ro>
2015-08-03 14:29:36 +0300

    calendar plus service

--- client/app/account/calendar/calendar.controller.js
+++ client/app/account/calendar/calendar.controller.js
@@ -1,7 +1,7 @@
 angular.module('schoolPlannerApp')
 
 .controller('CalendarCtrl',
-   function($scope, $compile, $timeout, uiCalendarConfig) {
+   function($scope, $compile, $timeout, uiCalendarConfig, CalendarService) {
     var date = new Date();
     var d = date.getDate();
     var m = date.getMonth();
@@ -15,14 +15,7 @@ angular.module('schoolPlannerApp')
             currentTimezone: 'America/Chicago' // an option!
     };
     /* event source that contains custom events on the scope */
-    $scope.events = [
-      {title: 'All Day Event',start: new Date(y, m, 1)},
-      {title: 'Long Event',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
-      {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false},
-      {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false},
-      {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false},
-      {title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
-    ];
+    $scope.events = CalendarService.getEvents();
     /* event source that calls a function on every view switch */
     $scope.eventsF = function (start, end, timezone, callback) {
       var s = new Date(start).getTime() / 1000;
