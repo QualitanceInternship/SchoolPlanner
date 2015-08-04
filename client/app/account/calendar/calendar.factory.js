3c21fa3a1d1fa54adbc5baa1a92d215253a57e75
Grigorescu Bogdan <grigorescu.bogdan.19@gmail.com>
2015-08-04 16:28:02 +0300

    Changes

---
+++ client/app/account/calendar/calendar.factory.js
@@ -0,0 +1,21 @@
+angular.module('schoolPlannerApp')
+  .factory('calendarFactory', function ($q, $http) {
+    return {
+      getEvents: function () {
+        var deferred = $q.defer(),
+          httpPromise = $http.get('/api/events');
+ 
+        httpPromise.success(function (components) {
+          var date = new Date()
+          var d = date.getDate();
+          var m = date.getMonth();
+          var y = date.getFullYear();
+          deferred.resolve(components);
+        })
+          .error(function (error) {
+            console.error('Error: ' + error);
+          });
+        return deferred.promise;
+      }
+    };
+  });
