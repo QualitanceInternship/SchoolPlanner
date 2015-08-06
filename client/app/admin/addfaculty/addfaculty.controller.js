b32589d1c31f2493b0ecfbf9cc3896ae18267a26
Oprisor Andrei <oprisor_andrei@yahoo.com>
2015-08-06 16:45:37 +0300

    add faculties

---
+++ client/app/admin/addfaculty/addfaculty.controller.js
@@ -0,0 +1,89 @@
+
+'use strict';
+
+  angular.module('schoolPlannerApp')
+  .controller('AddFacultyCtrl', function ($scope, $http, socket, $timeout) {
+    $scope.allFaculties = [];
+    $scope.displaySubjects = [];
+    $scope.saved=false;
+    $scope.allSubjects=[];
+    $scope.choice={}
+    $scope.choices=[];
+    $scope.teachers=[];
+    $scope.facultyname="";
+    $scope.name=[];
+
+    $http.get('/api/faculties').success(function(allFaculties) {
+      $scope.allFaculties = allFaculties;
+      socket.syncUpdates('faculties', $scope.allFaculties);
+
+    });
+    
+
+    $http.get('/api/subjects').success(function(allSubjects) {
+      $scope.allSubjects = allSubjects;
+      socket.syncUpdates('subject', $scope.allSubjects);
+      console.log('allSubjects: ', allSubjects);
+    });
+    
+   $http.get('/api/subjects').success(function(displaySubjects) {
+      $scope.displaySubjects = displaySubjects;
+      socket.syncUpdates('subject', $scope.displaySubjects);
+      console.log('displaySubjects: ', displaySubjects);
+    });
+
+
+    $scope.appendTeacher=function(name){
+      $scope.name=name;
+      if($scope.name.length===0) return;
+       for(var i=0; i<$scope.teachers.length; i++){
+        if($scope.name===$scope.teachers[i])
+          return;
+       }
+      $scope.teachers.push($scope.name);
+      $scope.name="";
+    };
+
+
+    $scope.appendChoice = function(myChoice){
+    var choice=angular.copy(myChoice);
+    
+    if($scope.allSubjects.length===0)
+     return;
+    else  $scope.choices.push(choice);
+    
+    for(var i=0; i<$scope.allSubjects.length; i++){
+      for(var j=0;j<$scope.choices.length; j++){
+        if($scope.allSubjects[i]._id===$scope.choices[j]){
+          $scope.allSubjects.splice(i,1);}
+      }}
+
+    };
+    
+ 
+
+    $scope.addFaculty = function() {
+    if($scope.facultyname==="") return;
+     console.log($scope.choices);
+      $http.post('/api/faculties', { 
+                                  name: $scope.facultyname,
+      								            professors: $scope.teachers,
+                                  subjects: $scope.choices,
+                                  active: true
+
+                                     });
+    
+      
+       $scope.facultyname="";
+       $scope.choices=null;
+       $scope.teachers=null;
+       $scope.saved=true;
+      $timeout(function(){$scope.saved=false}, 3000);
+    
+    };
+
+
+    $scope.$on('$destroy', function () {
+      socket.unsyncUpdates('faculties');
+    });
+  });
